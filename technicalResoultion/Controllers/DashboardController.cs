4b029ba645db77bcdaa2095719a4c1adbb8bc664
SoyMarioHdez-U <mario.hernandez2@catolica.edu.sv>
2024-05-13 18:19:53 -0600

    Cambios en el Layout y se ha agregado un ticket en el Dashboard

---
+++ technicalResoultion/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace technicalResoultion.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Title = "DASHBOARD";
+            return View();
+        }
+    }
+}
