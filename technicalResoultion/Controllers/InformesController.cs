09b1534a10cfaf249f54a7fa869725634c3d2ae1
SoyMarioHdez-U <mario.hernandez2@catolica.edu.sv>
2024-05-14 21:06:43 -0600

    Módulo: "Informes" - Se agregó la parte visual, hace falta hacerlo funcional con la base de datos.

---
+++ technicalResoultion/Controllers/InformesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace technicalResoultion.Controllers
+{
+    public class InformesController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Title = "INFORMES";
+            return View();
+        }
+    }
+}
